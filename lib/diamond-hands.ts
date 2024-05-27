5e33a040af95698903850e80cf93b13839629ebf
Borg <brussel17891@gmail.com>
2024-05-27 10:37:51 -0300

    Add diamond hands data

--- lib/diamond-hands.ts
+++ lib/diamond-hands.ts
@@ -1,11 +1,9 @@
 import type { Address } from 'viem'
 
+import prodDiamondHands from './diamond-hands-data.json'
 const devDiamondHands: Address[] = [
   '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',  // hardhat dev first wallet address
   '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
-]
-
-const prodDiamondHands: Address[] = [
 ]
 
 const diamondHands = new Set(process.env.NODE_ENV === 'production' ? prodDiamondHands : devDiamondHands)
